a5366d10e16e4ac8c78602474c8acdbb03dde249
Sona <sona2529001@gmail.com>
2026-02-24 17:27:30 +0530

    created IT page

--- CRM/Frontend/CRM-FRONTEND1-main/src/Pages/Login.js
+++ CRM/Frontend/CRM-FRONTEND1-main/src/Pages/Login.js
@@ -356,7 +356,7 @@ function Login() {
     if (user.role === "admin") navigate("/admin-dashboard");
 else if (user.role === "support") navigate("/support-dashboard");
 else if (user.role === "sales") navigate("/sales-dashboard");
-else if (user.role === "it") navigate("/it-dashboard");
+else if (user.role === "it") navigate("/tech-dashboard");
 else if (user.role === "hr") navigate("/hr-dashboard");
 else if (user.role === "digital-media") navigate("/digital-media-dashboard");
 else navigate("/dashboard");
