5276a9633a6c6d41e5e9159388b3c622ccc3f1c0
Divyanshu Kumar <divyanshu975677@gmail.com>
2026-07-23 17:11:11 +0530

    bug fix

--- node_modules/netcradus-crm-backend/config/db.js
+++ node_modules/netcradus-crm-backend/config/db.js
@@ -5,7 +5,8 @@ mongoose.set("bufferTimeoutMS", Number(process.env.MONGO_BUFFER_TIMEOUT_MS || 20
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const dbUri = process.env.MONGO_URI || "mongodb+srv://netcradus_db_user:gwQGRJs7Y4WFbHda@cluster0.zcdpx8c.mongodb.net/crm_db?retryWrites=true&w=majority";
+    const conn = await mongoose.connect(dbUri, {
       maxPoolSize: Number(process.env.MONGO_MAX_POOL_SIZE || 10),
       minPoolSize: Number(process.env.MONGO_MIN_POOL_SIZE || 0),
       serverSelectionTimeoutMS: Number(process.env.MONGO_SERVER_SELECTION_TIMEOUT_MS || 10000),
